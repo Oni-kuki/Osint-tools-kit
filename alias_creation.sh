ceb54c5925c12f7834f4420bade7bde214a855dd
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2022-07-11 15:00:35 +0200

    Create alias_creation.sh

---
+++ alias_creation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+alias hatcloud="/home/git-sources/Hatcloud" >> .bash_aliases
+alias o365chk="/home/git-sources/o365chk" >> .bash_aliases
+alias blacbkird="/home/git-sources/blackbird" >> .bash_aliases
+alias relationFB="/home/git-sources/RelationsFB" >> .bash_aliases
+alias SPY="/home/git-sources/SPY" >> .bash_aliases
+alias geowifi="/home/git-sources/geowifi" >> .bash_aliases
+alias osintgram="/home/git-sources/Osintgram" >> .bash_aliases
+alias infoga="/home/git-sources/Infoga" >> .bash_aliases
+
+source ~/.bash_aliases
