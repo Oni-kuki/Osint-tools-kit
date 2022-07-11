7df26f0a67f826f9716713210a77365f960009f3
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2022-07-11 16:47:13 +0200

    Update alias_creation.sh

--- alias_creation.sh
+++ alias_creation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  cd ~ && touch  ~/.bash_aliases
 
-echo 'alias hatcloud="ruby /home/git-sources/Hatcloud/hatcloud.rb" 
+echo 'alias hatcloud="ruby /home/git-sources/HatCloud/hatcloud.rb" 
 alias o365chk="python3 /home/git-sources/o365chk/o365chk.py"
-alias blacbkird="python3 /home/git-sources/blackbird/blackbird.py"
-alias webserver="python3 /home/git-sources/blackbird/webserver.py"
+alias blackbird="cd /home/git-sources/blackbird && python3 blackbird.py"
+alias webserver="cd /home/git-sources/blackbird && python3 webserver.py"
 alias relationFB="python3 /home/git-sources/RelationsFB/main.py"
 alias SPY="python3 /home/git-sources/SPY/spy.py"
 alias geowifi="python3 /home/git-sources/geowifi/geowifi.py"
