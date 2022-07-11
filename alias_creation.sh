f8111c7fa04aba5634f9d95d052b15b912914475
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2022-07-11 15:10:24 +0200

    Update alias_creation.sh

--- alias_creation.sh
+++ alias_creation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  cd ~ && touch  ~/.bash_aliases
  
-alias hatcloud="/home/git-sources/Hatcloud" >> .bash_aliases
-alias o365chk="/home/git-sources/o365chk" >> .bash_aliases
-alias blacbkird="/home/git-sources/blackbird" >> .bash_aliases
-alias relationFB="/home/git-sources/RelationsFB" >> .bash_aliases
-alias SPY="/home/git-sources/SPY" >> .bash_aliases
-alias geowifi="/home/git-sources/geowifi" >> .bash_aliases
-alias osintgram="/home/git-sources/Osintgram" >> .bash_aliases
-alias infoga="/home/git-sources/Infoga" >> .bash_aliases
+alias hatcloud="/home/git-sources/Hatcloud/hatcloud.rb" >> .bash_aliases
+alias o365chk="/home/git-sources/o365chk/o365chk.py" >> .bash_aliases
+alias blacbkird="/home/git-sources/blackbird/blackbird.py" >> .bash_aliases
+alias "webserver"="/home/git-sources/blackbird/webserver.py" >> .bash_aliases
+alias relationFB="/home/git-sources/RelationsFB/main.py" >> .bash_aliases
+alias SPY="/home/git-sources/SPY/spy.py" >> .bash_aliases
+alias geowifi="/home/git-sources/geowifi/geowifi.py" >> .bash_aliases
+alias osintgram="/home/git-sources/Osintgram/main.py" >> .bash_aliases
 
 source ~/.bash_aliases
